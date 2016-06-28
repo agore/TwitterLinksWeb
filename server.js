bc45680df9e47d28bb57e81d93152aa2eb2beb51
agore <agore@gannett.com>
2016-06-28 08:56:09 -0400

    added functionality to load more tweets

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ var pug = require('pug');
 var https = require("https");
 //var http = require("http");
 var momentTz = require("moment-timezone");
+var urlParser = require("url");
 
 app.set("port", (process.env.PORT || 3000));
 app.set("view engine", "pug");
 app.use("/js", express.static( __dirname + '/views/js'));
 app.get("/", function(request, response) {
     //http.get("http://localhost:5000/tweets/all", function(res) {
-    https.get("https://fierce-river-4730.herokuapp.com/tweets/all", function(res) {
+    https.get("https://fierce-river-4730.herokuapp.com/tweets/all?count=50", function(res) {
         var responseString = "";
         res.on("data", function(d) {
             //resp.render("index", {"tweets": d});
@@ -30,8 +31,23 @@ app.get("/", function(request, response) {
     }).end();
 });
 
+app.get("/more", function(request, response) {
+    var parts = urlParser.parse(request.url, true).query;
+    //console.log("count : " + parts.count);
+    //console.log("older: " + parts.older_than_id);
+    https.get("https://fierce-river-4730.herokuapp.com/tweets/all?count=" + parts.count + "&older_than_id=" + parts.older_than_id, function(res) {
+        var responseString = "";
+        res.on("data", function(d) {
+            responseString += d;
+        });
+
+        res.on("end", function() {
+            response.send(responseString);
+        });
+    });
+});
 
 var server = app.listen(app.get("port"), function() {
-    console.log("Server running on port ", app.get("port"));
+    //console.log("Server running on port ", app.get("port"));
 });
 
--- views/js/script.js
+++ views/js/script.js
@@ -3,17 +3,19 @@ $(document).ready(function() {
 });
 
 init = function() {
-    $("a").click(function() {
-        var $this = $(this);
-        markAsRead($this.data("tweet-id"), $this);
-    });
     $("div.tweet").click(function() {
        console.log("tweet clicked");
     });
+
+    $("a.loadMore").click(function() {
+        console.log("fetchMore");
+        var id = $("div.tweet:last").data("tweet-id");
+        fetchMore(id);
+    });
+
     markAsRead = function(tweetId, elem) {
         $.ajax({
-            _url: "https://fierce-river-4730.herokuapp.com/tweets/read",
-            url: "http://gci-agore-m.us.ad.gannett.com:5000/tweets/read",
+            url: "https://fierce-river-4730.herokuapp.com/tweets/read",
             data: JSON.stringify({
                 "id": tweetId
             }),
@@ -24,6 +26,34 @@ init = function() {
         }).error(function(jqXhr, status, error) {
             console.log("error is " + error);
         });
+    }
+
+    fetchMore = function(olderThanId) {
+        var requesId = olderThanId + 1;
+        console.log("Now fetching older than " + requesId);
+        
+        $.get({
+            _url: "https://fierce-river-4730.herokuapp.com/tweets/all?count=50&older_than_id=" + olderThanId,
+            url: "/more?count=50&older_than_id=" + requesId
+        }).success(function(data, textStatus, jqXhr) {
+            
+            var jsonResponse = JSON.parse(data);
+            for (var i = 0; i < jsonResponse.length; i++) {
+                var t = jsonResponse[i];
+                $t = $("<div class='tweet' data-tweet-id='" + t.id + "'>" +
+                        "    <img class='avatar' alt='" + t.name + "' src='" + t.au + "' />" +
+                        "    <div class='byline'><span class='name'>" + t.name + "</span><span class='handle'>@ " + t.sn + "</span></div>"+
+                        "    <div class='timestamp'>" + t.ts + "</div>" +
+                        "    <div class='text'>" + t.t + "</div>" +
+                        "    <ul class='links'><li class='link'><a href='" + t.urls[0]+ "' target='_new' data-tweet-id='" + t.id+ "'>" + t.urls[0]+ "</a></li></ul>" +
+                       "</div>");
+                $("div.tweet:last").after($t);
+            }
+            
+        }).error(function(jqXhr, status, error) {
+            console.log("error is " + error);
+        });
+        
     }
 }
 
